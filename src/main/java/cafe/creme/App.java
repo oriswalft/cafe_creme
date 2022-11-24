c1a558e407ec5d9392ce61dea7f03962b0d942a8
Oris Walft <46623531+oriswalft@users.noreply.github.com>
2022-11-24 10:33:39 +0100

    Encore une mise à jour

--- src/main/java/cafe/creme/App.java
+++ src/main/java/cafe/creme/App.java
@@ -5,6 +5,33 @@ import java.util.Scanner;
 public class App {
     
     public static void main(String[] args) {
+
+        //Compter les pièces
+        double totalPiecesJaunes = compterPieces();
+
+        //Afficher résultat final
+        afficherResulats(totalPiecesJaunes);
+
+
+
+
+    }
+
+    private static void afficherResulats(double totalPiecesJaunes) {
+        if (totalPiecesJaunes == 1) {
+            System.out.println("Vous avez exactement de quoi vous payez le café ! :D");
+        }
+        else if (totalPiecesJaunes < 1) {
+            double manquePiece = 1 - totalPiecesJaunes;
+            System.out.println("Il vous manque " + manquePiece +" euros pour prendre un café! D:");
+        }
+        else if (totalPiecesJaunes > 1) {
+            double pieceEnTrop = totalPiecesJaunes - 1;
+            System.out.println("Il vous restera "+ pieceEnTrop +" euros après avoir pris votre café ! :D");
+        }
+    }
+
+    private static double compterPieces() {
         System.out.println("Bonjour ! Je suis l'assistant de la machine à cafe. Dites moi le nombre de pièces jaunes que vous avez !");
         Scanner clavier = new Scanner(System.in);
         System.out.println("Entrez le nombre de pièces de 1 centimes: ");
@@ -22,22 +49,7 @@ public class App {
         clavier.close();
 
         double totalPiecesJaunes = piece1centime + piece2centime +piece5centime + piece10centime + piece20centime + piece50centime;
-
-        if (totalPiecesJaunes == 1) {
-            System.out.println("Vous avez exactement de quoi vous payez le café ! :D");
-        }
-        else if (totalPiecesJaunes < 1) {
-            double manquePiece = 1 - totalPiecesJaunes;
-            System.out.println("Il vous manque " + manquePiece +" euros pour prendre un café! D:");
-        }
-        else if (totalPiecesJaunes > 1) {
-            double pieceEnTrop = totalPiecesJaunes - 1;
-            System.out.println("Il vous restera "+ pieceEnTrop +" euros après avoir pris votre café ! :D");
-        }
-
-
-
-
+        return totalPiecesJaunes;
     }
     
 }
